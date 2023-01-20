59db6cad708da8aa2209352dc356626c2976735e
YeonHak Lee <dldusgkr788@gmail.com>
2023-01-20 15:01:00 +0900

    [FE] : [FEAT] 절대경로 설정 오직 src만 가능 public은 계속 상대경로 사용해야됨.

--- Frontend/marathon/src/pages/router-page/HeaderPage.jsx
+++ Frontend/marathon/src/pages/router-page/HeaderPage.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import Header from "../../components/common/Header.jsx";
-import Footer from "../../components/common/Footer.jsx";
+import Header from "components/common/Header.jsx";
 import { Outlet } from "react-router-dom";
 
 export default function HeaderPage() {
