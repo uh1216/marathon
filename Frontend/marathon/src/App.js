4e697becbe000b55cecce675df2932b8e76e192d
YeonHak Lee <dldusgkr788@gmail.com>
2023-01-20 11:23:42 +0900

    [FE] : [FEAT] HeaderFooter만 보이는 라우터 전용 페이지 / Header만 보이는 라우터 전용 페이지 생성 및 연결

--- Frontend/marathon/src/App.js
+++ Frontend/marathon/src/App.js
@@ -1,26 +1,27 @@
 import { Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import HeaderFooterPage from "./pages/router-page/HeaderFooterPage";
+import HeaderPage from "./pages/router-page/HeaderFooterPage";
 import Main from "./pages/Main";
 import Notice from "./pages/Notice";
-import Header from "./components/common/Header.jsx";
-import Footer from "./components/common/Footer.jsx";
 
 function App() {
   return (
     <>
-      <Header />
       <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/consult-enroll" element={<div>상담신청</div>} />
-        <Route path="/guide" element={<div>서비스 안내</div>} />
-        <Route path="/notice/*" element={<Notice />} />
-        <Route path="/self-study" element={<div>스스로 학습</div>} />
-        <Route path="/schedule-manage" element={<div>일정관리(선생)</div>} />
-        <Route path="/treat-enroll" element={<div>수업예약(환자)</div>} />
-        <Route path="/user" element={<div>로그인/회원가입</div>} />
-        <Route path="/mypage" element={<div>마이페이지</div>} />
+        <Route element={<HeaderFooterPage />}>
+          <Route path="/" element={<Main />} />
+          <Route path="/consult-enroll" element={<div>상담신청</div>} />
+          <Route path="/guide" element={<div>서비스 안내</div>} />
+          <Route path="/notice/*" element={<Notice />} />
+          <Route path="/self-study" element={<div>스스로 학습</div>} />
+          <Route path="/schedule-manage" element={<div>일정관리(선생)</div>} />
+          <Route path="/treat-enroll" element={<div>수업예약(환자)</div>} />
+          <Route path="/user" element={<div>로그인/회원가입</div>} />
+          <Route path="/mypage" element={<div>마이페이지</div>} />
+        </Route>
+        <Route element={<HeaderPage />}></Route>
       </Routes>
-      <Footer />
     </>
   );
 }
