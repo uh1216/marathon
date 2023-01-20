ea5c89d9f2677559d7f2c66e7bf3938265817231
YeonHak Lee <dldusgkr788@gmail.com>
2023-01-20 10:52:07 +0900

    [FE] : [FEAT] 설날에 만들 페이지 분할 스켈레톤 완성

--- Frontend/marathon/src/App.js
+++ Frontend/marathon/src/App.js
@@ -17,7 +17,7 @@ function App() {
         <Route path="/self-study" element={<div>스스로 학습</div>} />
         <Route path="/schedule-manage" element={<div>일정관리(선생)</div>} />
         <Route path="/treat-enroll" element={<div>수업예약(환자)</div>} />
-        <Route path="/member" element={<div>로그인/회원가입</div>} />
+        <Route path="/user" element={<div>로그인/회원가입</div>} />
         <Route path="/mypage" element={<div>마이페이지</div>} />
       </Routes>
       <Footer />
