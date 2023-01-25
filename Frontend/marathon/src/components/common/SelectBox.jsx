0f04dac4e81616bb188ade5115f7f67db61d9192
최준아 <jn307742@gmail.com>
2023-01-25 15:31:21 +0900

    [FE] : [FEAT] 회원가입 퍼블리싱 완료

--- Frontend/marathon/src/components/common/SelectBox.jsx
+++ Frontend/marathon/src/components/common/SelectBox.jsx
@@ -1,16 +1,18 @@
 import style from "./SelectBox.module.css";
+import React, { forwardRef, useRef } from "react";
 
 /**
  * 부모 컴포넌트에서는 다음과 같이 사용하기 (참고)
- * <SelectBox options={optionTest} onChange={(x) => selectTest(x)} width=""/>
+ * <SelectBox options={optionTest} onChange={(x) => selectTest(x)} width="" ref={sdajfsdl}/>
  * options : select 박스에 담고 싶은 옵션 객체 배열 (옵션 객체는 value와 name을 속성으로 가진다.)
  *
  * onChange : select 박스 선택값이 바꼈을 경우 실행되는 함수
  * x : 선택한 옵션의 value 값
  * width : select box 너비 (선택)
+ * ref : ref 변수명
  */
 
-export default function SelectBox(props) {
+const SelectBox = forwardRef((props, ref) => {
   /** Select Box에서 옵션 하나가 선택되었을 때,
    * 부모 컴포넌트에서 정의한 onChange 함수가
    * 선택된 옵션의 value값이 인자로 담겨서 실행됩니다.
@@ -24,10 +26,11 @@ export default function SelectBox(props) {
       className={style.select_box}
       onChange={handleChange}
       style={{ width: props.width }}
+      ref={ref}
     >
-      {props.options.map((option) => (
+      {props.options.map((option, idx) => (
         <option
-          key={option.value}
+          key={idx}
           value={option.value}
           defaultValue={props.defaultValue === option.value}
         >
@@ -36,4 +39,6 @@ export default function SelectBox(props) {
       ))}
     </select>
   );
-}
+});
+
+export default SelectBox;
--- Frontend/marathon/src/components/user/Login.jsx
+++ Frontend/marathon/src/components/user/Login.jsx
@@ -6,52 +6,69 @@ import ModalFindPwd from "./ModalFindPwd";
 
 export default function Main() {
   // 모달창 노출 여부 state
-  const [modalFindIdOpen, setModalFindIdOpen] = useState(false);
-  const [modalFindPwdOpen, setModalFindPwdOpen] = useState(false);
+  const [isModalFindIdOpen, setIsModalFindIdOpen] = useState(false);
+  const [isModalFindPwdOpen, setIsModalFindPwdOpen] = useState(false);
 
   // 모달창 노출
   const showModalFindId = () => {
-    setModalFindIdOpen(true);
+    setIsModalFindIdOpen(true);
   };
   const showModalFindPwd = () => {
-    setModalFindPwdOpen(true);
+    setIsModalFindPwdOpen(true);
   };
 
   return (
     <div className={style.user_box}>
-      <div className={style.title}>환영합니다</div>
-      <input className={style.input_text} type="text" placeholder="아이디" />
-      <input className={style.input_text} type="text" placeholder="비밀번호" />
-      <div>
+      <div className={style.inner_box}>
+        <div className={style.title}>환영합니다</div>
+        <input className={style.input_text} type="text" placeholder="아이디" />
         <input
-          className={style.memorize_id_box}
-          type="checkbox"
-          id="memorize_id_box"
+          className={style.input_text}
+          type="text"
+          placeholder="비밀번호"
         />
-        <label className={style.memorize_id_txt} htmlFor="memorize_id_box">
-          아이디 기억하기
-        </label>
-      </div>
-      <div>
-        <button className={`${style.btn} ${style.login}`}>로그인</button>
-        <img
-          className={style.btn}
-          src={Kakao_login_medium_wide}
-          alt="카카오 로그인 버튼"
-        />
-      </div>
-      <div className={style.find}>
-        <div className={style.inline} onClick={showModalFindId}>
-          아이디 찾기
+        {/* 아이디 기억하기 */}
+        <div className={style.memorize_id}>
+          <input
+            className={style.memorize_id_box}
+            type="checkbox"
+            id="memorize_id_box"
+          />
+          <label className={style.memorize_id_txt} htmlFor="memorize_id_box">
+            아이디 기억하기
+          </label>
         </div>
+        {/* 로그인 버튼 */}
+        <div>
+          <button className={`${style.btn} ${style.login}`}>로그인</button>
+          <img
+            className={style.btn}
+            src={Kakao_login_medium_wide}
+            alt="카카오 로그인 버튼"
+          />
+        </div>
+        {/* 아이디, 비밀번호 찾기 */}
+        <div className={style.find}>
+          <div
+            className={style.inline + " " + style.find_txt}
+            onClick={showModalFindId}
+          >
+            아이디 찾기
+          </div>
 
-        <div className={style.inline} onClick={showModalFindPwd}>
-          비밀번호 찾기
+          <div
+            className={style.inline + " " + style.find_txt}
+            onClick={showModalFindPwd}
+          >
+            비밀번호 찾기
+          </div>
+          {isModalFindIdOpen && (
+            <ModalFindId setModalOpen={setIsModalFindIdOpen} />
+          )}
+          {isModalFindPwdOpen && (
+            <ModalFindPwd setModalOpen={setIsModalFindPwdOpen} />
+          )}
         </div>
-        {modalFindIdOpen && <ModalFindId setModalOpen={setModalFindIdOpen} />}
-        {modalFindPwdOpen && (
-          <ModalFindPwd setModalOpen={setModalFindPwdOpen} />
-        )}
       </div>
     </div>
   );
