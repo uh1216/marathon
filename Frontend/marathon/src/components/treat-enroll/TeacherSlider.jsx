e3f76ff10eb0c6b5de84aa49c6bd77358a24eba8
kjskjs356 <kjskjs356@naver.com>
2023-01-27 12:17:41 +0900

    [FE] : [FIX] 수업예약페이지 캐러셀 기능 보완

--- Frontend/marathon/src/components/treat-enroll/TeacherSlider.jsx
+++ Frontend/marathon/src/components/treat-enroll/TeacherSlider.jsx
@@ -2,47 +2,64 @@ import styled from "styled-components";
 import style from "./TeacherSlider.module.css";
 
 const NowSlider = styled.div`
-  width: ${(props) => (props.check === "now" ? "20%" : "15%")};
-  height: ${(props) => (props.check === "now" ? "400px" : "200px")};
-  top: ${(props) => (props.check === "now" ? "55%" : "50%")};
+  width: ${(props) => (props.check === "now" ? "20%" : "18%")};
+  height: ${(props) => (props.check === "now" ? "450px" : "400px")};
+  top: ${(props) => (props.check === "now" ? "55%" : "55%")};
+  visibility: ${(props) =>
+    props.check.includes("hidden") ? "hidden" : "visible"};
+  opacity: ${(props) => (props.check.includes("hidden") ? "0" : "1")};
+  border: 1px solid #c0c0c0;
+  border-radius: 5%;
+  background-image: ${(props) =>
+    props.check === "now"
+      ? "linear-gradient(to bottom, white 40%, #F3F7FF 40%)"
+      : "linear-gradient(to bottom, white 45%, #F3F7FF 45%)"};
   transform: translate(-50%, -50%);
-  transition: all ease-in-out 0.5s;
+  transition: ${(props) =>
+    (props.check === "now" && "all ease-in-out 0.5s") ||
+    (props.check === "next" && "all ease-in-out 0.5s") ||
+    (props.check === "prev" && "all ease-in-out 0.5s") ||
+    (props.check === "hidden_next" && "all ease-in-out 0.5s") ||
+    (props.check === "hidden_prev" && "all ease-in-out 0.5s")};
   position: absolute;
+
+  animation: ${(props) =>
+    props.check.includes("hidden") ? "fadeout 0.5s" : "fadein 0.5s"};
 
   left: ${(props) =>
     (props.check === "now" && "50%") ||
-    (props.check === "next" && "70%") ||
-    (props.check === "prev" && "30%")};
+    (props.check === "next" && "72%") ||
+    (props.check === "prev" && "28%") ||
+    (props.check === "hidden_next" && "95%") ||
+    (props.check === "hidden_prev" && "5%")};
 `;
 
 export default function TeacherSlider({ check, name, content, bg }) {
   return (
     <>
-      {check !== "hidden" ? (
-        <NowSlider check={check}>
-          <div className={style.total}>
-            <div className={style.info}>
-              <img src={bg} alt="" className={style.teacher_img}></img>
-              <h3 className={style.title}>
-                {name}&nbsp;
-                <span className={style.span}>재활사</span>
-              </h3>
-            </div>
-            <div className={style.button}>
-              <button
-                className={style.button_design}
-                disabled={check === "now" ? false : true}
-              >
-                예약하기
-              </button>
-            </div>
-            <div className={style.content}>
-              <h3>자기소개</h3>
-              {content}
-            </div>
+      <NowSlider check={check}>
+        <div className={style.total}>
+          <div className={style.info}>
+            <img src={bg} alt="" className={style.teacher_img}></img>
+            <h3 className={style.title}>
+              {name}&nbsp;
+              <span className={style.span}>재활사</span>
+            </h3>
           </div>
-        </NowSlider>
-      ) : null}
+          <div className={style.button}>
+            <button
+              className={style.button_design}
+              disabled={check === "now" ? false : true}
+            >
+              예약하기
+            </button>
+          </div>
+          <div className={style.content}>
+            <h3 className={style.introduce_title}>자기소개</h3>
+            {content}
+          </div>
+        </div>
+      </NowSlider>
     </>
   );
 }
