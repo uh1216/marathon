31d896e031f7b1f537f533613d59542282ea8f76
최준아 <jn307742@gmail.com>
2023-01-30 17:20:10 +0900

    [FE] : [FIX] 스스로 학습 게임 페이지 - 점수가 초기화되지 않는 에러 해결

--- Frontend/marathon/src/components/self-study/self-study-game/EasyMode1.jsx
+++ Frontend/marathon/src/components/self-study/self-study-game/EasyMode1.jsx
@@ -11,10 +11,11 @@ export default function EasyMode1() {
 
   useEffect(() => {
     /** 1단계라면 점수 기록을 초기화 */
-    if (stage === 1) dispatch(resetRecord());
+    if (stage == 1) {
+      dispatch(resetRecord());
+    }
 
     /** 정답을 맞췄다면 아래 코드 실행 */
-    console.log(stage);
     dispatch(addRecord(true));
 
     /** 정답을 틀렸다면 아래 코드 실행 */
