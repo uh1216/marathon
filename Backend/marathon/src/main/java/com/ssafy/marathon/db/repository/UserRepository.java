6b0a37f90c0a4175b02773a774ed57fe2e491833
rlaehddus815 <rlaehddus815@gmail.com>
2023-01-31 16:24:20 +0900

    [BE] : [FEAT] 재활사의 예약테이블 불러오기 기능 완성(doctor부분 미완성)

--- Backend/marathon/src/main/java/com/ssafy/marathon/db/repository/UserRepository.java
+++ Backend/marathon/src/main/java/com/ssafy/marathon/db/repository/UserRepository.java
@@ -1,8 +1,12 @@
 package com.ssafy.marathon.db.repository;
 
+import com.ssafy.marathon.db.entity.user.Doctor;
 import com.ssafy.marathon.db.entity.user.User;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository<T extends User> extends JpaRepository<T, Long> {
+
+    Doctor findDoctorBySeq(Long seq);
+
 }
