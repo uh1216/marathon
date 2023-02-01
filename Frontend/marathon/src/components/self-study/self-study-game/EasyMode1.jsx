a8d876accabb33a04b4e061cd7a804fa9063bc63
최준아 <jn307742@gmail.com>
2023-02-01 14:28:38 +0900

    [FE] : [FEAT] 게임 새로고침 방지 기능 추가

--- Frontend/marathon/src/components/self-study/self-study-game/EasyMode1.jsx
+++ Frontend/marathon/src/components/self-study/self-study-game/EasyMode1.jsx
@@ -9,6 +9,37 @@ export default function EasyMode1() {
 
   const dispatch = useDispatch();
 
+  const preventGoBack = (e) => {
+    console.log(e);
+    let isGoBack = window.confirm("종료하기를 눌러주세요 :D");
+    // if (!isGoBack) {
+    //   window.history.pushState(null, "", "");
+    // }
+    // if (isGoBack) {
+    //   window.history.popState();
+    // }
+  };
+
+  // 새로고침 막기 변수
+  const preventClose = (e) => {
+    e.preventDefault();
+    e.returnValue = ""; // chrome에서는 설정이 필요해서 넣은 코드
+  };
+
+  // 브라우저에 렌더링 시 한 번만 실행하는 코드
+  useEffect(() => {
+    (() => {
+      window.history.pushState(null, "", "");
+      window.addEventListener("popstate", preventGoBack);
+      window.addEventListener("beforeunload", preventClose);
+    })();
+
+    return () => {
+      window.removeEventListener("popstate", preventGoBack);
+      window.removeEventListener("beforeunload", preventClose);
+    };
+  }, []);
+
   useEffect(() => {
     /** 1단계라면 점수 기록을 초기화 */
     if (gameState.stage == 1 && gameState.isReady) {
