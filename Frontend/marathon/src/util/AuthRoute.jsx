0e7803243312df22d811b00a28b23610004c12f1
YeonHak Lee <dldusgkr788@gmail.com>
2023-02-01 17:47:09 +0900

    [FE] : [FEAT] mypage 여러가지

--- Frontend/marathon/src/util/AuthRoute.jsx
+++ Frontend/marathon/src/util/AuthRoute.jsx
@@ -2,7 +2,7 @@ import { useNavigate } from "react-router-dom";
 
 export default function AuthRoute(props) {
   const navigate = useNavigate();
-  const authenticated = false;
+  const authenticated = true;
 
   if (!authenticated) {
     alert("로그인이 필요한 페이지입니다.");
