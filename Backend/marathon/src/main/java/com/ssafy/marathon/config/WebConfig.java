985b2a5445853a578d9e9baee53f13dbef7b0ae4
uh1216 <ruh2034@naver.com>
2023-02-02 15:26:01 +0900

    [BE] : [FEAT] CORS 설정 파일 추가

--- Backend/marathon/src/main/java/com/ssafy/marathon/config/WebConfig.java
+++ Backend/marathon/src/main/java/com/ssafy/marathon/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://www.localhost:3000")
+            .allowedOrigins("*")
             .allowedMethods("*");
 
     }
