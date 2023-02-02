fd6ff86f8507074ace4528650b1fb753e9e62358
hosanyoon <yoonhosan@naver.com>
2023-02-02 15:16:51 +0900

    [BE] : [FEAT] 공지사항 초기생성

--- Backend/marathon/src/main/java/com/ssafy/marathon/init/BoardInit.java
+++ Backend/marathon/src/main/java/com/ssafy/marathon/init/BoardInit.java
@@ -1,32 +1,31 @@
 package com.ssafy.marathon.init;
 
+import com.ssafy.marathon.db.entity.board.Board;
 import com.ssafy.marathon.db.entity.user.Admin;
 import com.ssafy.marathon.db.repository.AdminRepository;
+import com.ssafy.marathon.db.repository.BoardRepository;
 import java.util.Collections;
 import javax.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.info.ProjectInfoProperties.Build;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @RequiredArgsConstructor
 @Component
-public class AdminInit {
-
+public class BoardInit {
     private final Logger LOGGER = LoggerFactory.getLogger(AdminInit.class);
-    private final AdminRepository adminRepository;
-    private final PasswordEncoder passwordEncoder;
 
+    private final BoardRepository boardRepository;
     @PostConstruct
     protected void init() {
-        LOGGER.info("[init] admin 유저생성 시작");
-        Admin admin = Admin.builder()
-            .id("ssafy").password(passwordEncoder
-                .encode("ssafy"))
-            .roles(Collections.singletonList("ROLE_ADMIN")).build();
-        admin.setImg("default.PNG");
-        adminRepository.save(admin);
-        LOGGER.info("[init] admin 유저생성 완료 id : {}", admin.getId());
+        LOGGER.info("[init] 공지사항 생성 시작");
+        for (int i = 0; i < 30; i++) {
+            Board board = Board.builder().title(i + "번째 공지").content(i + "번째 내용").build();
+            boardRepository.save(board);
+        }
+        LOGGER.info("[init] 공지사항 완료 ");
     }
 }
