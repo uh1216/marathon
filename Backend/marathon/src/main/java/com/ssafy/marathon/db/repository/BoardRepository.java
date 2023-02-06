2eb280cdeadb939ed7253526c98bdc0c04dfd02b
uh1216 <ruh2034@naver.com>
2023-02-06 16:02:10 +0900

    [BE] : [FIX] 상담 수정2

--- Backend/marathon/src/main/java/com/ssafy/marathon/db/repository/BoardRepository.java
+++ Backend/marathon/src/main/java/com/ssafy/marathon/db/repository/BoardRepository.java
@@ -1,7 +1,6 @@
 package com.ssafy.marathon.db.repository;
 
 import com.ssafy.marathon.db.entity.board.Board;
-import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,4 +8,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
     Page<Board> findAllByOrderByRegistDateDesc(Pageable pageable);
+
+    Page<Board> findAllByTitleContainingOrderByRegistDateDesc(String condition, Pageable pageable);
+
+    Page<Board> findAllByContentContainingOrderByRegistDateDesc(String condition,
+        Pageable pageable);
+
+    Page<Board> findAllByContentContainingOrTitleContainingOrderByRegistDateDesc(
+        String contentCondition, String titleCondition, Pageable pageable);
 }
