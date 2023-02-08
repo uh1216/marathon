fe9d0bf4c02a6fb3c63a83487c33fd7ddc6b5e92
최준아 <jn307742@gmail.com>
2023-02-08 13:12:55 +0900

    [FE] : [FEAT] 화상 치료 레이아웃 짜는 중

--- Frontend/marathon/src/pages/Treat.jsx
+++ Frontend/marathon/src/pages/Treat.jsx
@@ -1,3 +1,16 @@
+import style from "./Treat.module.css";
+
 export default function Treat() {
-  return <></>;
+  return (
+    <div className={style.wrapper}>
+      <div style={{ width: "474px", height: "530px" }}>
+        <img
+          style={{ width: "474px", height: "530px", objectFit: "cover" }}
+          src="https://pickcon.co.kr/site/data/img_dir/2022/06/29/2022062980010_0.jpg"
+          alt="임시 이미지"
+        />
+      </div>
+      <div className={style.btn_container}></div>
+    </div>
+  );
 }
