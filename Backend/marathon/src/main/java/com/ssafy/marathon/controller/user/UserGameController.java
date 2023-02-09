fd9ad6d40fed3b640ea5e5b30062c4af5f2d453e
최준아 <jn307742@gmail.com>
2023-02-09 16:49:55 +0900

    [FE] : [FEAT] 웹소켓 종류별로 생성

--- Backend/marathon/src/main/java/com/ssafy/marathon/controller/user/UserGameController.java
+++ Backend/marathon/src/main/java/com/ssafy/marathon/controller/user/UserGameController.java
@@ -14,11 +14,63 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserGameController {
     private final SimpMessagingTemplate simpMessagingTemplate;
 
-    @MessageMapping("/hello")
-    public void sendMessage(@RequestBody InteractionReqDto interactionReqDto, SimpMessageHeaderAccessor accessor) {
+//    채팅
+    @MessageMapping("/chat")
+    public void sendChat(@RequestBody InteractionReqDto interactionReqDto, SimpMessageHeaderAccessor accessor) {
         System.out.println("--------------------");
         System.out.println(interactionReqDto.getContent());
-        simpMessagingTemplate.convertAndSend("/topic/roomId", interactionReqDto);
+        System.out.println("/chat/" + interactionReqDto.getChannelId());
+        simpMessagingTemplate.convertAndSend("/chat/" + interactionReqDto.getChannelId(), interactionReqDto);
+//        simpMessagingTemplate.convertAndSend("/topic/roomId" + interactionReqDto.getChannelId(), interactionReqDto);
+    }
+
+//    스케치보드
+    @MessageMapping("/sketch")
+    public void sendSketch(@RequestBody InteractionReqDto interactionReqDto, SimpMessageHeaderAccessor accessor) {
+        System.out.println("--------------------");
+        System.out.println(interactionReqDto.getContent());
+        System.out.println("/sketch/" + interactionReqDto.getChannelId());
+        simpMessagingTemplate.convertAndSend("/sketch/" + interactionReqDto.getChannelId(), interactionReqDto);
+//        simpMessagingTemplate.convertAndSend("/topic/roomId" + interactionReqDto.getChannelId(), interactionReqDto);
+    }
+
+//    무작위 질문
+    @MessageMapping("/question")
+    public void sendQuestion(@RequestBody InteractionReqDto interactionReqDto, SimpMessageHeaderAccessor accessor) {
+        System.out.println("--------------------");
+        System.out.println(interactionReqDto.getContent());
+        System.out.println("/question/" + interactionReqDto.getChannelId());
+        simpMessagingTemplate.convertAndSend("/question/" + interactionReqDto.getChannelId(), interactionReqDto);
+//        simpMessagingTemplate.convertAndSend("/topic/roomId" + interactionReqDto.getChannelId(), interactionReqDto);
+    }
+
+//    프리셋
+    @MessageMapping("/preset")
+    public void sendPreset(@RequestBody InteractionReqDto interactionReqDto, SimpMessageHeaderAccessor accessor) {
+        System.out.println("--------------------");
+        System.out.println(interactionReqDto.getContent());
+        System.out.println("/preset/" + interactionReqDto.getChannelId());
+        simpMessagingTemplate.convertAndSend("/preset/" + interactionReqDto.getChannelId(), interactionReqDto);
+//        simpMessagingTemplate.convertAndSend("/topic/roomId" + interactionReqDto.getChannelId(), interactionReqDto);
+    }
+
+//    그림 보드
+    @MessageMapping("/image")
+    public void sendImage(@RequestBody InteractionReqDto interactionReqDto, SimpMessageHeaderAccessor accessor) {
+        System.out.println("--------------------");
+        System.out.println(interactionReqDto.getContent());
+        System.out.println("/image/" + interactionReqDto.getChannelId());
+        simpMessagingTemplate.convertAndSend("/image/" + interactionReqDto.getChannelId(), interactionReqDto);
+//        simpMessagingTemplate.convertAndSend("/topic/roomId" + interactionReqDto.getChannelId(), interactionReqDto);
+    }
+
+//    끝말잇기
+    @MessageMapping("/wordChain")
+    public void sendWordChain(@RequestBody InteractionReqDto interactionReqDto, SimpMessageHeaderAccessor accessor) {
+        System.out.println("--------------------");
+        System.out.println(interactionReqDto.getContent());
+        System.out.println("/wordChain/" + interactionReqDto.getChannelId());
+        simpMessagingTemplate.convertAndSend("/wordChain/" + interactionReqDto.getChannelId(), interactionReqDto);
 //        simpMessagingTemplate.convertAndSend("/topic/roomId" + interactionReqDto.getChannelId(), interactionReqDto);
     }
 }
