1a3597300ea7eb4d49ab3888c4d7aec91077075c
rlaehddus815 <rlaehddus815@gmail.com>
2023-02-09 15:11:23 +0900

    [BE]:[FIX] API FIX

--- Backend/marathon/src/main/java/com/ssafy/marathon/service/doctor/DoctorTreatmentService.java
+++ Backend/marathon/src/main/java/com/ssafy/marathon/service/doctor/DoctorTreatmentService.java
@@ -26,5 +26,5 @@ public interface DoctorTreatmentService {
 
     void deleteTreatment(Long treatmentSeq);
 
-    void makeAlarmAndDelTreatment(Long patientSeq, String sessionId, Long doctorSeq);
+    void makeAlarmAndDelTreatment(Long treatmentSeq, String sessionId, Long doctorSeq);
 }
