d5bc2106618d16a5939b1144b412199a5d132dfd
wnsdk <jn307742@gmail.coom>
2023-02-09 21:04:36 +0900

    [FE] : [FEAT] 채팅 기능 + 회원 정보 이미지 변경

--- Frontend/marathon/src/components/treat/Chatting.jsx
+++ Frontend/marathon/src/components/treat/Chatting.jsx
@@ -1,15 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import style from "./Chatting.module.css";
 
-export default function Chatting({ isChatting, stompClient, channelId }) {
+export default function Chatting({
+  isChatting,
+  stompClient,
+  channelId,
+  chatList,
+}) {
   const [message, setMessage] = useState("");
+
+  useEffect(() => {}, []);
+
   /** 전송 클릭 시 실행 */
   const submit = () => {
     if (message === "") return;
     stompClient.send(
-      "/changeInteraction",
+      "/chat",
       {},
-      JSON.stringify({ channelId: channelId, content: message })
+      JSON.stringify({
+        channelId: channelId,
+        content: message,
+        sender: sessionStorage.getItem("access-token"),
+      })
     );
     setMessage("");
   };
@@ -17,82 +29,35 @@ export default function Chatting({ isChatting, stompClient, channelId }) {
   return (
     <div className={style.wrapper} style={{ visibility: `${isChatting}` }}>
       <div className={style.chatting_container}>
-        <div className={style.your_wrapper}>
-          <img
-            className={style.your_bubble_profile}
-            src="https://newsimg.sedaily.com/2021/12/09/22V85NTJGY_1.jpg"
-            alt=""
-          />
-          <div>
-            <div className={style.your_name}>김김김</div>
-            <div className={style.your_bubble_container}>
-              <span className={style.your_bubble}>
-                <div className={style.your_bubble_tail}></div>
-                <span className={style.your_bubble_content}>?????????</span>
+        {chatList.map((item) =>
+          item.hasOwnProperty("senderImg") ? (
+            <div className={style.your_wrapper}>
+              <img
+                className={style.your_bubble_profile}
+                src={item.senderImg}
+                alt=""
+              />
+              <div>
+                <div className={style.your_name}>{item.senderName}</div>
+                <div className={style.your_bubble_container}>
+                  <span className={style.your_bubble}>
+                    <div className={style.your_bubble_tail}></div>
+                    <span className={style.your_bubble_content}>
+                      {item.content}
+                    </span>
+                  </span>
+                </div>
+              </div>
+            </div>
+          ) : (
+            <div className={style.my_bubble_container}>
+              <span className={style.my_bubble}>
+                <div className={style.my_bubble_tail}></div>
+                <span className={style.my_bubble_content}>{item.content}</span>
               </span>
             </div>
-          </div>
-        </div>
-        <div className={style.my_bubble_container}>
-          <span className={style.my_bubble}>
-            <div className={style.my_bubble_tail}></div>
-            <span className={style.my_bubble_content}>?????????</span>
-          </span>
-        </div>
-        <div className={style.my_bubble_container}>
-          <span className={style.my_bubble}>
-            <div className={style.my_bubble_tail}></div>
-            <span className={style.my_bubble_content}>네~~!!</span>
-          </span>
-        </div>
-        <div className={style.my_bubble_container}>
-          <span className={style.my_bubble}>
-            <div className={style.my_bubble_tail}></div>
-            <span className={style.my_bubble_content}>네~~!!</span>
-          </span>
-        </div>
-        <div className={style.my_bubble_container}>
-          <span className={style.my_bubble}>
-            <div className={style.my_bubble_tail}></div>
-            <span className={style.my_bubble_content}>네~~!!</span>
-          </span>
-        </div>
-        <div className={style.my_bubble_container}>
-          <span className={style.my_bubble}>
-            <div className={style.my_bubble_tail}></div>
-            <span className={style.my_bubble_content}>네~~!!</span>
-          </span>
-        </div>
-        <div className={style.my_bubble_container}>
-          <span className={style.my_bubble}>
-            <div className={style.my_bubble_tail}></div>
-            <span className={style.my_bubble_content}>네~~!!</span>
-          </span>
-        </div>
-        <div className={style.my_bubble_container}>
-          <span className={style.my_bubble}>
-            <div className={style.my_bubble_tail}></div>
-            <span className={style.my_bubble_content}>네~~!!</span>
-          </span>
-        </div>
-        <div className={style.my_bubble_container}>
-          <span className={style.my_bubble}>
-            <div className={style.my_bubble_tail}></div>
-            <span className={style.my_bubble_content}>네~~!!</span>
-          </span>
-        </div>
-        <div className={style.my_bubble_container}>
-          <span className={style.my_bubble}>
-            <div className={style.my_bubble_tail}></div>
-            <span className={style.my_bubble_content}>네~~!!</span>
-          </span>
-        </div>
-        <div className={style.my_bubble_container}>
-          <span className={style.my_bubble}>
-            <div className={style.my_bubble_tail}></div>
-            <span className={style.my_bubble_content}>네~~!!</span>
-          </span>
-        </div>
+          )
+        )}
       </div>
       <hr />
       <div className={style.textarea_container}>
@@ -101,6 +66,11 @@ export default function Chatting({ isChatting, stompClient, channelId }) {
           value={message}
           onChange={(e) => {
             setMessage(e.target.value);
+          }}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              submit();
+            }
           }}
         ></textarea>
         <div className={style.submit} onClick={submit}>
