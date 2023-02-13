0abf3f11dc3278da59827994338c4f86ece814e4
rlaehddus815 <rlaehddus815@gmail.com>
2023-02-13 09:14:08 +0900

    [BE]:[FIX] BUG FIX

--- Backend/marathon/src/main/java/com/ssafy/marathon/db/entity/treatment/Reservation.java
+++ Backend/marathon/src/main/java/com/ssafy/marathon/db/entity/treatment/Reservation.java
@@ -2,15 +2,9 @@ package com.ssafy.marathon.db.entity.treatment;
 
 import com.ssafy.marathon.db.entity.user.Doctor;
 import java.time.LocalDate;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
-import lombok.*;
+import javax.persistence.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import lombok.*;
 
 @Getter
 @Setter
