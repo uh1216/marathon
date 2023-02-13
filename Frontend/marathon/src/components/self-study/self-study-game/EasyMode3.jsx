c9e47d9ab13f7a9f1b91770e03f9074d305d6fcb
최준아 <jn307742@gmail.com>
2023-02-13 12:11:28 +0900

    [FE] : [FEAT] 게임 소리 넣기

--- Frontend/marathon/src/components/self-study/self-study-game/EasyMode3.jsx
+++ Frontend/marathon/src/components/self-study/self-study-game/EasyMode3.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addRecord, resetRecord, setType } from "stores/game.store";
 import commonStyle from "./Game.module.css";
@@ -24,12 +24,18 @@ export default function EasyMode1() {
   const [myAnswer, setMyAnswer] = useState([]);
   const [stageResult, setStageResult] = useState();
   const navigate = useNavigate();
+  const refClickSound = useRef();
 
   // 모바일일때 돌아가게 만들기
   const isMobile = () => {
     return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
       navigator.userAgent
     );
+  };
+
+  /** 클릭 시 효과음 */
+  const playClickSound = () => {
+    refClickSound.current.play();
   };
 
   /** (min <= 값 < max) 랜덤숫자 뽑기 */
@@ -346,6 +352,7 @@ export default function EasyMode1() {
   } else if (gameState.isReady === 1) {
     return (
       <>
+        <audio ref={refClickSound} src={clickSound} />
         <div className={commonStyle.stage}>{gameState.stage} /10</div>
         <div className={commonStyle.title}>
           원래 위치로 동물을 가져다 놓으세요!
@@ -363,6 +370,7 @@ export default function EasyMode1() {
                 draggable
                 pre_y="-1"
                 pre_x="-1"
+                onClick={playClickSound}
               >
                 {animals[i]}
               </button>
