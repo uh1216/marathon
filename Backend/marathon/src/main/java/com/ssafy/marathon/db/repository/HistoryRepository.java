688c8709952a89ad8488d204eafce19a0a6bd58b
uh1216 <ruh2034@naver.com>
2023-02-14 14:33:07 +0900

    [BE]:[FIX] 메시지 보낼 사람 가져오기 수정

--- Backend/marathon/src/main/java/com/ssafy/marathon/db/repository/HistoryRepository.java
+++ Backend/marathon/src/main/java/com/ssafy/marathon/db/repository/HistoryRepository.java
@@ -1,20 +1,27 @@
 package com.ssafy.marathon.db.repository;
 
 import com.ssafy.marathon.db.entity.treatment.History;
-
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface HistoryRepository extends JpaRepository<History, Long> {
+
     History findBySeq(Long historySeq);
-    History findByDoctor_SeqAndPatient_SeqAndDateAndTime(Long doctorSeq, Long patientSeq, LocalDate localDate, LocalTime localTime);
+
+    History findByDoctor_SeqAndPatient_SeqAndDateAndTime(Long doctorSeq, Long patientSeq,
+        LocalDate localDate, LocalTime localTime);
+
     List<History> findAllByDoctor_SeqAndFeedbackEquals(Long doctorSeq, String emptyString);
+
     List<History> findAllByDoctor_Seq(Long doctorSeq);
+
+    List<History> findDistinctByDoctor_SeqAndDateBetween(Long doctorSeq, LocalDate start,
+        LocalDate end);
+
+    List<History> findDistinctByPatient_SeqAndDateBetween(Long patientSeq, LocalDate start,
+        LocalDate end);
 
     List<History> findAllByPatient_Seq(Long patientSeq);
 
